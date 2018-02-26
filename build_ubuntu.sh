a2844f620ebbcec81e2181904ead209cf056d357
Inho Oh <webispy@gmail.com>
2018-02-26 17:15:32 +0900

    ubuntu: Add qemu-aarch64/arm-static to cross env
    Signed-off-by: Inho Oh <webispy@gmail.com>

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -23,11 +23,11 @@ case "$1" in
 		;;
 
 	"u710cross")
-		docker exec -t $1 bash -c "mk-sbuild --target arm64 xenial && sudo sed -i 's/^union-type=.*/union-type=overlay/' /etc/schroot/chroot.d/sbuild-xenial-amd64-arm64 && sbuild-update xenial-amd64-arm64 && sbuild-upgrade xenial-amd64-arm64"
+		docker exec -t $1 bash -c "mk-sbuild --target arm64 xenial && sudo sed -i 's/^union-type=.*/union-type=overlay/' /etc/schroot/chroot.d/sbuild-xenial-amd64-arm64 && sbuild-update xenial-amd64-arm64 && sbuild-upgrade xenial-amd64-arm64 && sudo cp /usr/bin/qemu-aarch64-static /var/lib/schroot/chroots/xenial-amd64-arm64/usr/bin"
 		;;
 
 	"u530cross")
-		docker exec -t $1 bash -c "mk-sbuild --target armhf xenial && sudo sed -i 's/^union-type=.*/union-type=overlay/' /etc/schroot/chroot.d/sbuild-xenial-amd64-armhf && sbuild-update xenial-amd64-armhf && sbuild-upgrade xenial-amd64-armhf"
+		docker exec -t $1 bash -c "mk-sbuild --target armhf xenial && sudo sed -i 's/^union-type=.*/union-type=overlay/' /etc/schroot/chroot.d/sbuild-xenial-amd64-armhf && sbuild-update xenial-amd64-armhf && sbuild-upgrade xenial-amd64-armhf && sudo cp /usr/bin/qemu-arm-static /var/lib/schroot/chroots/xenial-amd64-armhf/usr/bin"
 		;;
 	*)
 		exit 1
